299a0dd549f73af8b77170e6ccdfc69faff515ce
yawar-abass <bhatsharik6146@gmail.com>
2024-05-28 12:43:36 +0530

    changes in env

--- src/lib/actions/image.actions.ts
+++ src/lib/actions/image.actions.ts
@@ -96,7 +96,7 @@ export async function getAllImages({
 
     cloudinary.config({
       cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+      api_key: process.env.CLOUDINARY_API_KEY,
       api_secret: process.env.CLOUDINARY_API_SECRET,
       secure: true,
     });
